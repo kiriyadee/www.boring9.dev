b48ff5be2467a6ddcef18a26d4bde4b27b995831
EW <wit.chanthawat@gmail.com>
2024-10-20 19:18:17 +0700

    Refactor app/head.tsx: Update page description

--- app/head.tsx
+++ app/head.tsx
@@ -13,7 +13,7 @@ const Head = () => {
     title: `Chanthawat Kiriyadee ${
       pathname === '/' ? '' : '· ' + titleFixedToShow
     }`,
-    description: `Hi! My name is Chanthawat Kiriyadee I'm from Thailand and I have +2 year of work experience in JavaScript/TypeScript and React, I'm a lover of page design or other things and whenever I can I try to learn new technologies since I discovered programming at 16. I love what I do and I help others with what I can, I also use in web, mobile and desktop development Next.js, Angular, Ionic and Electron implementing good practices, clean architecture, pixel perfect and agile methodologies.`,
+    description: `Hi! My name is Chanthawat Kiriyadee. I'm a software engineer. I write about web development, JavaScript, TypeScript, React, and more.`,
     keywords: 'Chanthawat Kiriyadee',
     type: 'website'
   };
